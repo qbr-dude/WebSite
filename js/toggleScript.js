72eb495f25321008dcf46c758c12b35c36b9f196
qbr-dude <mr.jaydi@mail.ru>
2020-12-13 14:18:01 +0300

    Authorization form completed

--- js/toggleScript.js
+++ js/toggleScript.js
@@ -1,16 +1,36 @@
-let loginFormState = false;
-let loginForm = document.getElementById('login-form');
+let loginDivState = false;
+let registrationState = false;
+
+let loginDiv = document.getElementById('login-div');
 let loginRef = document.getElementById('login-ref');
+let loginForm = document.getElementById('login-form');
+let registrationForm = document.getElementById('registration-form');
 
 function toggleLoginForm() {
-    if(loginForm) {
-        loginForm.classList.toggle('profile-form-inactive');
+    if(loginDivState) {
+        loginDiv.classList.toggle('profile-form-inactive');
         loginRef.classList.toggle('profile-form-active-ref');
-        loginFormState = false;
+        loginForm.classList.remove('profile-form-inactive');
+        registrationForm.classList.add('profile-form-inactive');
+        registrationState = false;
+        loginDivState = false;
     } 
     else {
         loginRef.classList.toggle('profile-form-active-ref');
+        loginDiv.classList.toggle('profile-form-inactive');
+        loginDivState = true;
+    }
+}
+
+function toggleRegistration() {
+    if(registrationState) {
         loginForm.classList.toggle('profile-form-inactive');
-        loginFormState = true;
+        registrationForm.classList.toggle('profile-form-inactive');
+        registrationState = false;
+    }
+    else {
+        loginForm.classList.toggle('profile-form-inactive');
+        registrationForm.classList.toggle('profile-form-inactive');
+        registrationState = true;
     }
 }
